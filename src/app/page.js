daa6d901c0d05ba479df6266b5b20e3bc048a8de
Sasa Savic Perusina <sasa@bubulazy.com>
2023-09-12 12:33:14 +0100

    Playing around with structure of proyect

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <h1 className={styles.title}>
         Practicando Next.js
       </h1>
