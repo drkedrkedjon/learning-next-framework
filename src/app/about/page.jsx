5714716dc29ce06a814e81a0062ff61187183c6e
Sasa Savic Perusina <sasa@bubulazy.com>
2023-09-13 12:40:07 +0100

    Learning dinamic routes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>About</h1>
       <Link href="/">Back to Home Page</Link>
     </div>
   );
---
+++ src/app/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: 1,
+    name: "Sasa"
+  },
+  {
+    id: 2,
+    name: "Sisi"
+  },
+  {
+    id: 3,
+    name: "Susu"
+  }
+]
