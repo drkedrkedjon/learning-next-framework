94f16b9a440ec5174596f7f899596ce59cecdf3d
Sasa Savic Perusina <sasa@bubulazy.com>
2023-09-14 12:55:51 +0100

    Learning Image compenent

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
+import myImage from "@/app/images/equipo.jpg";
 
 export default function About() {
   return (
     <div className={styles.wrapper}>
+      <Image src={myImage} alt="equipo" />
       <h1 className={styles.title}>About</h1>
       <Link href="/">Back to Home Page</Link>
     </div>
