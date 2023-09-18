02a9ff8f54e0d638dd572275566861333873e585
Sasa Savic Perusina <sasa@bubulazy.com>
2023-09-18 12:39:32 +0100

    Pkaying with Framer Motion animate

--- src/app/framer/page.js
+++ src/app/framer/page.js
@@ -1,12 +1,18 @@
+"use client"
+import { useState } from "react"
 import styles from "./page.module.css"
+import { motion } from "framer-motion"
 
 export default function FramerMotion() {
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.h1}>Framer Page</h1>
-      <div className={styles.ball}>
-
-      </div>
+      <motion.div
+        animate={{
+          y: -80,
+          scale: 2,
+        }}
+        className={styles.ball} />
       <button className={styles.btn}>Click me baa</button>
     </div>
   )
