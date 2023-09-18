280657edb094355a1f3dbd8e8d767791c43f03b4
Sasa Savic Perusina <sasa@bubulazy.com>
2023-09-18 13:03:54 +0100

    Playing with Framer Motion transition

--- src/app/framer/page.js
+++ src/app/framer/page.js
@@ -2,18 +2,28 @@
 import { useState } from "react"
 import styles from "./page.module.css"
 import { motion } from "framer-motion"
+import Link from "next/link"
 
 export default function FramerMotion() {
+  const [isActive, setIsActive] = useState(true)
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.h1}>Framer Page</h1>
       <motion.div
+        initial={false}
+        transition={{
+          type: "spring",
+          stiffness: 500,
+          damping: 8,
+        }}
         animate={{
-          y: -80,
-          scale: 2,
+          y: isActive ? 0 : -40,
+          scale: isActive ? 1 : 1.4,
         }}
         className={styles.ball} />
-      <button className={styles.btn}>Click me baa</button>
+      <button onClick={() => setIsActive(!isActive)} className={styles.btn}>Click me baa... !</button>
+      <Link href={"/"} >Back to home</Link>
     </div>
   )
 }
